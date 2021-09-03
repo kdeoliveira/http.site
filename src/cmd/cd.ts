1187cd8b72b6fa68ab271908bf524eed93fd2f25
Kevin de Oliveira <contact@kevindeoliveira.com>
2021-09-03 17:03:11 -0400

    frontend

--- src/cmd/cd.ts
+++ src/cmd/cd.ts
@@ -1,62 +1,74 @@
-import type {TreeDirectory} from "../context/command.context";
+import type { TreeDirectory } from "../context/terminal.context";
+import { changeTreeDirectory } from "../store/action";
 import { CommandFunction } from "./cmd.type";
 
-const cd : CommandFunction = (args: string, tree: TreeDirectory) => {
-    
+const cd: CommandFunction = (args: string, tree: TreeDirectory) => {
 
-    if(!args || args === "/") return {
-        tree: {
-            ...tree,
-            current: {
-                name: "root",
-                type: "folder",
-                path: ""
-            }
-        },
-        value: "",
-        status: "fetched"
+
+    // if(!args || args === "/") return {
+    //     tree: {
+    //         ...tree,
+    //         current: {
+    //             name: "root",
+    //             type: "folder",
+    //             path: ""
+    //         }
+    //     },
+    //     value: "",
+    //     status: "fetched"
+    // }
+    if (!args || args === "/") {
+        changeTreeDirectory({
+            name: "root",
+            type: "folder",
+            path: ""
+        });
+
+        return {
+            value: "",
+            status: "fetched"
+        }
     }
 
 
     const str = args.trim().split("/");
 
-    if(str[0].length === 0) throw new Error("Incorrect path")
+    if (str[0].length === 0) throw new Error("Incorrect path")
 
 
-    const key : string[] = tree.current.path.split("/").filter((x: any) => x);
+    const key: string[] = tree.current.path.split("/").filter((x: any) => x);
 
     str.forEach((x) => {
-        if(x === "..")
+        if (x === "..")
             key.pop();
-        else if(x)
+        else if (x)
             key.push(x);
     })
-    
-    let temp : any = tree.tree;
+
+    let temp: any = tree.tree;
 
 
 
-    key.forEach((x : string) => {
-        if(x && temp[x] && temp[x].type === "folder")
+    key.forEach((x: string) => {
+        if (x && temp[x] && temp[x].type === "folder")
             temp = temp[x]
-        else   
+        else
             throw new Error(`${args}: Not a directory`)
     })
 
-    if(temp === undefined) throw new Error(`${args}: Not such directory`)
+    if (temp === undefined) throw new Error(`${args}: Not such directory`)
+
+    changeTreeDirectory({
+        name: temp.name,
+        type: temp.type,
+        path: key.join("/")
+    });
+
     return {
-            value: "",
-            tree: {
-                ...tree,
-                current : {
-                    name: temp.name,
-                    type: temp.type,
-                    path: key.join("/")
-                }
-            },
-            status: "fetched"
-        }
-        
+        value: "",
+        status: "fetched"
+    }
+
 }
 
 
